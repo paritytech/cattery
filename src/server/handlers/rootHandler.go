b1826f0830051fd0ebf43374366568bdc1daec30
Evgeny Snitko <evgeny@parity.io>
2026-04-30 15:06:35 +0400

    healthcheck (#56)

--- src/server/handlers/rootHandler.go
+++ src/server/handlers/rootHandler.go
@@ -13,6 +13,18 @@ type Handlers struct {
 	ScaleSetManager *scaleSetPoller.Manager
 }
 
-func (h *Handlers) Index(responseWriter http.ResponseWriter, r *http.Request) {
-	http.Redirect(responseWriter, r, "/status", http.StatusFound)
+func (h *Handlers) Index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("cattery\n"))
+}
+
+func (h *Handlers) StatusIndex(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/status", http.StatusFound)
+}
+
+func (h *Handlers) Healthcheck(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("ok\n"))
 }
